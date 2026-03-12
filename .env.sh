9b05197be7b816310cd78d0201c032ed1f35b621
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-03-12 01:32:33 +0000

    semaphore

--- .env.sh
+++ .env.sh
@@ -1,7 +1,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ -f "${DIR}/.env" ]; then
+if [[ -f "${DIR}/.env" ]]; then
     # https://unix.stackexchange.com/a/79065
     eval "$(/bin/bash "${DIR}/bash/exportsource.sh" "${DIR}/.env")"
 fi
@@ -11,7 +11,7 @@ fi
 # -----------------------------------------------------------------------------
 
 # check also if PROJECT_NAME exist
-if [ -z "${PROJECT_NAME}" ]; then
+if [[ -z "${PROJECT_NAME}" ]]; then
     echo "${0} error: PROJECT_NAME is not set"
     exit 1
 fi
@@ -19,7 +19,7 @@ fi
 re='^[0-9]+$'
 
 # Validate PORT
-if [ -z "${PORT}" ]; then
+if [[ -z "${PORT}" ]]; then
     echo "${0} error: PORT is not set"
     exit 1
 fi
@@ -30,7 +30,7 @@ if ! [[ "${PORT}" =~ ${re} ]]; then
 fi
 
 # Validate HOST
-if [ -z "${HOST}" ]; then
+if [[ -z "${HOST}" ]]; then
     echo "${0} error: HOST is not set"
     exit 1
 fi
