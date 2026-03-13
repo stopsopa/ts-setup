472b3728a8f4c2de0f16b3514042005548780bc8
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-03-13 00:46:51 +0000

    transpile

--- public/semaphore/Semaphore.test.js
+++ public/semaphore/Semaphore.test.js
@@ -5,32 +5,32 @@ import Semaphore from "../../src/test-examples/Semaphore.ts";
  *
  */
 test("Semaphore", () => {
-    const semaphore = new Semaphore(3);
-    semaphore.acquire();
-    semaphore.acquire();
-    semaphore.acquire();
-    semaphore.acquire();
-    semaphore.release();
-    semaphore.release();
-    semaphore.release();
-    semaphore.release();
-    assert.strictEqual(semaphore.permits, 3);
+  const semaphore = new Semaphore(3);
+  semaphore.acquire();
+  semaphore.acquire();
+  semaphore.acquire();
+  semaphore.acquire();
+  semaphore.release();
+  semaphore.release();
+  semaphore.release();
+  semaphore.release();
+  assert.strictEqual(semaphore.permits, 3);
 });
 test("Semaphore - order", async () => {
-    const semaphore = new Semaphore(3);
-    const promises = [];
-    const order = [];
-    async function doStuff(id) {
-        await semaphore.acquire();
-        order.push(id);
-        await new Promise((resolve) => setTimeout(resolve, Math.random() * 100 + 900));
-        // await new Promise((resolve) => setTimeout(resolve, 1000));
-        semaphore.release();
-    }
-    for (let i = 0; i < 10; i++) {
-        promises.push(doStuff(i));
-    }
-    await Promise.all(promises);
-    assert.strictEqual(semaphore.permits, 3);
-    assert.deepStrictEqual(order, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const semaphore = new Semaphore(3);
+  const promises = [];
+  const order = [];
+  async function doStuff(id) {
+    await semaphore.acquire();
+    order.push(id);
+    await new Promise((resolve) => setTimeout(resolve, Math.random() * 100 + 900));
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    semaphore.release();
+  }
+  for (let i = 0; i < 10; i++) {
+    promises.push(doStuff(i));
+  }
+  await Promise.all(promises);
+  assert.strictEqual(semaphore.permits, 3);
+  assert.deepStrictEqual(order, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 });
